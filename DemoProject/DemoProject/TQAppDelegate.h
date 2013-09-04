8a53fc9c7525fae5507e1340b0698515d594d2c7
Pawan Rai <ghz.rai@gmail.com>
2013-09-04 15:52:49 +0530

    Demo comitt
    Demo Change

---
+++ DemoProject/DemoProject/TQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TQAppDelegate.h
+//  DemoProject
+//
+//  Created by Pawan Rai on 9/4/13.
+//  Copyright (c) 2013 AppStudioz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoProject/DemoProject/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DemoProject
+//
+//  Created by Pawan Rai on 9/4/13.
+//  Copyright (c) 2013 AppStudioz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TQAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TQAppDelegate class]));
+    }
+}
